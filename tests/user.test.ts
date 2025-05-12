50a73628bbdb9d900d6ca3051716941a02c3a5c3
Thenoob24 <constant.blaszyk.cb@gmail.com>
2025-05-12 13:44:36 +0200

    fix user création test

--- tests/user.test.ts
+++ tests/user.test.ts
@@ -4,53 +4,50 @@ import { prismaMock } from './jest.setup';
 import { describe, it, expect, jest } from '@jest/globals';
 import bcrypt from 'bcryptjs';
 
-async function getToken() {
-  const response = await request(app).post('/users/login').send({
-    email: 'testuser@example.com',
-    password: 'password123'
-  });
-  return response.body.token;
-}
 
 describe('User API', () => {
   describe('POST /users', () => {
     it('should create a new user', async () => {
       const createdUser = {
+        id: 1,
         email: 'testuser@example.com',
         password: 'password123'
       };
-      prismaMock.user.create.mockResolvedValue({
-        id: 1,
-        ...createdUser,
-        password: await bcrypt.hash(createdUser.password, 10)
-      });
 
-      const response = await request(app).post('/users').send(createdUser);
+      prismaMock.user.create.mockResolvedValue(createdUser);
 
-      expect(response.status).toBe(201);
-      expect(response.body.email).toEqual(createdUser.email);
+      const res = await request(app)
+        .post('/users')
+        .send(createdUser);
+        
+        expect(res.status).toBe(201);
+        expect(res.body).toEqual({
+          id: createdUser.id,
+          email: createdUser.email,
+        });
     });
   });
 
   describe('POST /users/login', () => {
     it('should login a user', async () => {
-      const user = {
+      const userLogin = {
         id: 1,
         email: 'testuser@example.com',
-        password: await bcrypt.hash('password123', 10)
+        password: 'password123',
       };
-      prismaMock.user.findUnique.mockResolvedValue(user);
+      console.log('User from database:', userLogin); // Debug
 
-      const response = await request(app).post('/users/login').send({
-        email: 'testuser@example.com',
-        password: 'password123'
-      });
+      prismaMock.user.findUnique.mockResolvedValue(userLogin);
 
-      console.log('User from database:', user); // Debug
-      console.log('Response:', response.body); // Debug
+      const res = await request(app)
+      .post('/users/login')
+      .send(userLogin);
 
-      expect(response.status).toBe(200);
-      expect(response.body).toEqual({ token: expect.any(String) });
+      console.log('Response:', res.body); // Debug
+
+      expect(res.status).toBe(200);
+      expect(res.body).toEqual({ token: expect.any(String) });
+
     });
   });
 });
